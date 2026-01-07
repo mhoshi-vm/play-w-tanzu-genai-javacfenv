b1ea34e2db8fe0ec43bd67fa6e8e3ed5f23fd87b
mhoshi-vm <machi.hoshino@broadcom.com>
2026-01-07 12:59:11 +0900

    Initial commit

---
+++ src/main/java/jp/broadcom/tanzu/mhoshi/playwtanzugenaijavacfenv/PlayWTanzuGenaiJavacfenvApplication.java
@@ -0,0 +1,13 @@
+package jp.broadcom.tanzu.mhoshi.playwtanzugenaijavacfenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWTanzuGenaiJavacfenvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayWTanzuGenaiJavacfenvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/broadcom/tanzu/mhoshi/playwtanzugenaijavacfenv/PlayWTanzuGenaiJavacfenvApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.broadcom.tanzu.mhoshi.playwtanzugenaijavacfenv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayWTanzuGenaiJavacfenvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
